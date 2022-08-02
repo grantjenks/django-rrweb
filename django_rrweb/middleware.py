91563d7548f3deb183b5e4ab5fbceeb9bca2bb96
Grant Jenks <grant.jenks@gmail.com>
2022-08-01 20:19:36 -0700

    Create middleware for rrweb session keys across pages

---
+++ django_rrweb/middleware.py
@@ -0,0 +1,16 @@
+from uuid import uuid4
+
+
+def session_key_middleware(get_response):
+
+    def middleware(request):
+        session_key = request.COOKIES.get('rrweb-session-key')
+
+        if session_key is None:
+            session_key = str(uuid4())
+
+        response = get_response(request)
+        response.set_cookie('rrweb-session-key', session_key)
+        return response
+
+    return middleware
--- tests/www/settings.py
+++ tests/www/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_rrweb.middleware.session_key_middleware',
 ]
 
 ROOT_URLCONF = 'www.urls'
