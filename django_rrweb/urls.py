c92f69f13e247b72a530485058db0b7b7cc63438
Grant Jenks <grant.jenks@gmail.com>
2022-08-01 20:17:49 -0700

    Support replay by session key

--- django_rrweb/urls.py
+++ django_rrweb/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('record/', views.record),
-    path('replay/<session_key>/', views.replay),
+    path('record/', views.record, name='rrweb-record'),
+    path(
+        'replay/<session_key>/',
+        views.replay_session,
+        name='rrweb-replay-session',
+    ),
 ]
