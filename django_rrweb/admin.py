a79084998504ce9e43ddfe0bd97c9fe06683e25e
Grant Jenks <grant.jenks@gmail.com>
2022-08-01 20:16:50 -0700

    Remove add/delete permission and add replay link

--- django_rrweb/admin.py
+++ django_rrweb/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Event, Session
 
@@ -17,7 +19,25 @@ class SessionAdmin(admin.ModelAdmin):
         'duration',
         'event_count',
         'event_size',
+        'replay',
     ]
+
+    def has_add_permission(self, request):
+        return False
 
     def has_change_permission(self, request, obj=None):
         return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    @admin.display(
+        description='View',
+        ordering='session_key',
+    )
+    def replay(self, obj):
+        url = reverse(
+            'rrweb-replay-session',
+            kwargs={'session_key': obj.session_key},
+        )
+        return format_html('<a href="{url}">view</a>', url=url)
