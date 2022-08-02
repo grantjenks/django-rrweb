2235177ae7865decdcd2ef7782a11ad7649e8cb1
Grant Jenks <grant.jenks@gmail.com>
2022-08-01 22:33:28 -0700

    i sort it

--- tests/www/urls.py
+++ tests/www/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('backend/rrweb/', include('django_rrweb.urls')),
     path('backend/', admin.site.urls),
