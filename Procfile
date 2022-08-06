900d361eb62585511f9fa5e5436ad9dd2a08ffdd
Grant Jenks <grant.jenks@gmail.com>
2022-08-05 23:44:57 -0700

    Setup www app and test gunicorn ... okay!

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn www.wsgi --workers 2 --threads 2 --log-file -
+release: python manage.py migrate
--- www/setup.py
+++ www/setup.py
@@ -1,10 +1,18 @@
+import pathlib
+import re
+
 import setuptools
+
+www_dir = pathlib.Path(__file__).parent
+init = (www_dir.parent / 'django_rrweb' / '__init__.py').read_text()
+match = re.search(r"^__version__ = '(.+)'$", init, re.MULTILINE)
+version = match.group(1)
 
 setuptools.setup(
     name='www',
-    version=todo,
+    version=version,
     install_requires=[
-        'Django=3.2.*',
+        'Django==3.2.*',
         'dj-database-url',
         'gunicorn',
         'psycopg2-binary',
