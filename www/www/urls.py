44d7dcf6fdd1bbe9f0956c11b0462cc825975249
Grant Jenks <grant.jenks@gmail.com>
2022-08-06 11:47:21 -0700

    Add index for dokku health check

--- www/www/urls.py
+++ www/www/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
+    path('', views.index),
     path('backend/', admin.site.urls),
 ]
