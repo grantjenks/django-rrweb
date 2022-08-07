5e84f8828c784ceed93fdfc2fe731c205c20292c
Grant Jenks <grant.jenks@gmail.com>
2022-08-07 16:56:31 -0700

    Remove csrf token handling

--- django_rrweb/templates/django-rrweb/record-script.js
+++ django_rrweb/templates/django-rrweb/record-script.js
@@ -1,21 +1,4 @@
 (function () {
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== "") {
-            const cookies = document.cookie.split(";");
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + "=")) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    const csrfToken = getCookie("csrftoken");
     let sessionKey = "{{ request.session.rrweb_session_key }}";
     let rrwebEvents = [];
 
@@ -32,7 +15,6 @@
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "X-CSRFToken": csrfToken,
             },
             mode: "cors",
             body,
