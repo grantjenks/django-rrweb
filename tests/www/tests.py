84c4ece68e31dddefc66ca1d4eb54cc7486eb579
Grant Jenks <gjenks@linkedin.com>
2022-08-16 12:04:37 -0700

    Test all paths when for record script

--- tests/www/tests.py
+++ tests/www/tests.py
@@ -2,6 +2,8 @@ import pytest
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+from django_rrweb.models import Event, Session
+
 pytestmark = pytest.mark.django_db
 
 
@@ -13,8 +15,19 @@ class ReplayTestCase(TestCase):
         )
 
     def test_record_script(self):
+        self.assertFalse(self.client.session.get('django_rrweb_session_key'))
         response = self.client.get('/backend/django_rrweb/record/script/')
         self.assertEqual(response.status_code, 200)
+        key = self.client.session['django_rrweb_session_key']
+        self.assertTrue(key)
+        response = self.client.get('/backend/django_rrweb/record/script/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(key, self.client.session['django_rrweb_session_key'])
+        session = Session.objects.create(key=key)
+        Event.objects.create(session=session, kind=0, data='', timestamp=1)
+        response = self.client.get('/backend/django_rrweb/record/script/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(key, self.client.session['django_rrweb_session_key'])
 
     def test_record(self):
         data = {
@@ -32,12 +45,6 @@ class ReplayTestCase(TestCase):
                 },
             ],
         }
-        response = self.client.post(
-            '/backend/django_rrweb/record/events/',
-            data,
-            content_type='application/json',
-        )
-        self.assertEqual(response.status_code, 200)
         response = self.client.post(
             '/backend/django_rrweb/record/events/',
             data,
