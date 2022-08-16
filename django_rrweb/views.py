3245d03ddc55fb6dc44cbf488d2c9b6e545ea055
Grant Jenks <gjenks@linkedin.com>
2022-08-16 12:06:51 -0700

    i sort it

--- django_rrweb/views.py
+++ django_rrweb/views.py
@@ -3,8 +3,8 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone as tz
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Event, Session
 
