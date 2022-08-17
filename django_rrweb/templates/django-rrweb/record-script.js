efe5b5bbdb79ae013c2f57a4b8e671680aa74da5
Grant Jenks <grant.jenks@gmail.com>
2022-08-16 22:42:17 -0700

    Linter improvements and bug fixes

--- django_rrweb/templates/django-rrweb/record-script.js
+++ django_rrweb/templates/django-rrweb/record-script.js
@@ -3,23 +3,23 @@
         let rrwebSessionKey = window.rrwebSessionKey || "{{ session_key }}";
         let rrwebEvents = [];
 
-        rrwebRecord({
+        window.rrwebRecord({
             emit(event) {
                 rrwebEvents.push(event);
-            },
+            }
         });
 
         function rrwebSave() {
-            const body = JSON.stringify({ rrwebSessionKey, rrwebEvents });
+            const body = JSON.stringify({ rrwebEvents, rrwebSessionKey });
             rrwebEvents = [];
             fetch("//{{ request.get_host }}{% url 'django-rrweb-record-events' %}", {
+                body,
                 credentials: "include",
                 headers: {
-                    "Content-Type": "application/json",
+                    "Content-Type": "application/json"
                 },
                 method: "POST",
-                mode: "cors",
-                body,
+                mode: "cors"
             });
         }
 
@@ -30,11 +30,11 @@
 
     if (window.rrwebRecord === undefined) {
         const scriptTag = document.createElement("script");
-        scriptTag.onload = rrwebSetup
+        scriptTag.onload = rrwebSetup;
         scriptTag.src = "//{{ request.get_host }}{% static 'django-rrweb/rrweb-record-1-1-3.js' %}";
         document.head.appendChild(scriptTag);
     }
     else {
         rrwebSetup();
     }
-})();
+}());
