d2163d3d0485dde223f8b43a3c3a0c06b64f33fb
Grant Jenks <grant.jenks@gmail.com>
2022-08-16 22:40:58 -0700

    Load rrweb recorder script if not already present

--- django_rrweb/templates/django-rrweb/record-script.js
+++ django_rrweb/templates/django-rrweb/record-script.js
@@ -1,28 +1,40 @@
-(function () {
-    let rrwebSessionKey = window.rrwebSessionKey || "{{ session_key }}";
-    let rrwebEvents = [];
+{% load static %}(function () {
+    function rrwebSetup() {
+        let rrwebSessionKey = window.rrwebSessionKey || "{{ session_key }}";
+        let rrwebEvents = [];
 
-    rrwebRecord({
-        emit(event) {
-            rrwebEvents.push(event);
-        },
-    });
-
-    function rrwebSave() {
-        const body = JSON.stringify({ rrwebSessionKey, rrwebEvents });
-        rrwebEvents = [];
-        fetch("//{{ request.get_host }}{% url 'django-rrweb-record-events' %}", {
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
+        rrwebRecord({
+            emit(event) {
+                rrwebEvents.push(event);
             },
-            method: "POST",
-            mode: "cors",
-            body,
         });
+
+        function rrwebSave() {
+            const body = JSON.stringify({ rrwebSessionKey, rrwebEvents });
+            rrwebEvents = [];
+            fetch("//{{ request.get_host }}{% url 'django-rrweb-record-events' %}", {
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                mode: "cors",
+                body,
+            });
+        }
+
+        setInterval(rrwebSave, 1000);
+        window.addEventListener("beforeunload", rrwebSave);
+        window.addEventListener("unload", rrwebSave);
     }
 
-    setInterval(rrwebSave, 1000);
-    window.addEventListener('beforeunload', rrwebSave);
-    window.addEventListener('unload', rrwebSave);
+    if (window.rrwebRecord === undefined) {
+        const scriptTag = document.createElement("script");
+        scriptTag.onload = rrwebSetup
+        scriptTag.src = "//{{ request.get_host }}{% static 'django-rrweb/rrweb-record-1-1-3.js' %}";
+        document.head.appendChild(scriptTag);
+    }
+    else {
+        rrwebSetup();
+    }
 })();
