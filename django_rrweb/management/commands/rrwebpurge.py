b305d14f7737d14de5e3dd9c1c2a692ce710b753
Grant Jenks <grant.jenks@gmail.com>
2022-08-30 22:53:39 -0700

    Add rrweb purge management command

---
+++ django_rrweb/management/commands/rrwebpurge.py
@@ -0,0 +1,48 @@
+import datetime as dt
+import logging
+import time
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone as tz
+
+from ...models import Page, Session
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Purges pages and sessions'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--delay', default=600)
+        parser.add_argument('--limit', default=float('inf'))
+        parser.add_argument('--max-age', default=7)
+
+    def handle(self, *args, **options):
+        delay = options['delay']
+        limit = options['limit']
+        max_age = dt.timedelta(days=options['max_age'])
+        count = 0
+
+        while True:
+            now = tz.now()
+            past = now - max_age
+            sessions = Session.objects.filter(
+                notes='',
+                pages__notes='',
+                create_time__lt=past,
+            )
+            log.warning('Deleting %s sessions', sessions.count())
+            sessions.delete()
+            pages = Page.objects.filter(
+                notes='',
+                session__notes='',
+                create_time__lt=past,
+            )
+            log.warning('Deleting %s pages', pages.count())
+            pages.delete()
+            count += 1
+            if count >= limit:
+                break
+            log.info('Sleeping for %s seconds', delay)
+            time.sleep(delay)
